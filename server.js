16599a8bea123701c342feaa660acb9b83e94e3d
Arash <erani.arash@gmail.com>
2018-11-08 16:32:19 -0800

    working proxy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 const path = require("path");
 const request = require("request");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
