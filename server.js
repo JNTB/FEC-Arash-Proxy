2c1a5e9deb6a097db2da5eda2de1d7c2d9981193
Arash <erani.arash@gmail.com>
2018-11-10 14:19:33 -0800

    proxy

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ const port = 3000;
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(`/pics`, function(req, res) {
-  var url = `http://localhost:1130/pics`;
-  req.pipe(request(url)).pipe(res);
-});
+// app.use(`/pics`, function(req, res) {
+//   var url = `http://localhost:1130/pics`;
+//   req.pipe(request(url)).pipe(res);
+// });
 
-app.use(`/api/similar`, function(req, res) {
-  var url = `http://localhost:1128/api/similar`;
-  req.pipe(request(url)).pipe(res);
-});
+// app.use(`/api/similar`, function(req, res) {
+//   var url = `http://localhost:1128/api/similar`;
+//   req.pipe(request(url)).pipe(res);
+// });
 
-app.use(`/reviews/:id`, function(req, res) {
-  var url = `http://localhost:2000/reviews/${req.params.id}`;
-  req.pipe(request(url)).pipe(res);
-});
+// app.use(`/reviews/:id`, function(req, res) {
+//   var url = `http://localhost:2000/reviews/${req.params.id}`;
+//   req.pipe(request(url)).pipe(res);
+// });
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
